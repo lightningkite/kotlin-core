afcaaea062e721e6c9f3e9510b6ca8f3bc3f1c39
Joseph Ivie <josephivie@gmail.com>
2016-08-12 00:20:23 -0600

    Simple upgrades!  Serialization working

--- src/main/java/com/lightningkite/kotlin/collection/Cache.kt
+++ src/main/java/com/lightningkite/kotlin/collection/Cache.kt
@@ -5,12 +5,11 @@ import java.util.*
 /**
  * Created by josep on 2/12/2016.
  */
-class Cache<K, V>(val map: MutableMap<K, V> = HashMap(), val generate: (K) -> V) : MutableMap<K, V> by map {
-    override fun get(key: K): V {
-        return map[key] ?: {
-            val new = generate(key)
-            map[key] = new
-            new
-        }()
+open class Cache<K, V>(val map: MutableMap<K, V> = HashMap(), val generate: (K) -> V?) : MutableMap<K, V> by map {
+    override fun get(key: K): V? {
+        return map[key] ?: generate(key).apply {
+            if (this != null)
+                map[key] = this
+        }
     }
 }
