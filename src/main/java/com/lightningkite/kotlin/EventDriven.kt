016446d8d5eb78782b088f3bcc620fcab7f0f0f1
Joseph Ivie <josephivie@gmail.com>
2017-07-25 13:33:43 -0600

    Chat finished, various items

--- src/main/java/com/lightningkite/kotlin/EventDriven.kt
+++ src/main/java/com/lightningkite/kotlin/EventDriven.kt
@@ -5,31 +5,37 @@ package com.lightningkite.kotlin
  * Created by josep on 5/31/2016.
  */
 
-inline fun Iterable<() -> Unit>.runAll() {
+@Deprecated("Use invokeAll instead.", ReplaceWith("invokeAll()")) inline fun Iterable<() -> Unit>.runAll() = invokeAll()
+
+inline fun Iterable<() -> Unit>.invokeAll() {
     for (item in this) {
         item()
     }
 }
 
-inline fun <A> Iterable<(A) -> Unit>.runAll(a: A) {
+@Deprecated("Use invokeAll instead.", ReplaceWith("invokeAll(a)")) inline fun <A> Iterable<(A) -> Unit>.runAll(a: A) = invokeAll(a)
+inline fun <A> Iterable<(A) -> Unit>.invokeAll(a: A) {
     for (item in this) {
         item(a)
     }
 }
 
-inline fun <A, B> Iterable<(A, B) -> Unit>.runAll(a: A, b: B) {
+@Deprecated("Use invokeAll instead.", ReplaceWith("invokeAll(a, b)")) inline fun <A, B> Iterable<(A, B) -> Unit>.runAll(a: A, b: B) = invokeAll(a, b)
+inline fun <A, B> Iterable<(A, B) -> Unit>.invokeAll(a: A, b: B) {
     for (item in this) {
         item(a, b)
     }
 }
 
-inline fun <A, B, C> Iterable<(A, B, C) -> Unit>.runAll(a: A, b: B, c: C) {
+@Deprecated("Use invokeAll instead.", ReplaceWith("invokeAll(a, b, c)")) inline fun <A, B, C> Iterable<(A, B, C) -> Unit>.runAll(a: A, b: B, c: C) = invokeAll(a, b, c)
+inline fun <A, B, C> Iterable<(A, B, C) -> Unit>.invokeAll(a: A, b: B, c: C) {
     for (item in this) {
         item(a, b, c)
     }
 }
 
-inline fun <A, B, C, D> Iterable<(A, B, C, D) -> Unit>.runAll(a: A, b: B, c: C, d: D) {
+@Deprecated("Use invokeAll instead.", ReplaceWith("invokeAll(a, b, c, d)")) inline fun <A, B, C, D> Iterable<(A, B, C, D) -> Unit>.runAll(a: A, b: B, c: C, d: D) = invokeAll(a, b, c, d)
+inline fun <A, B, C, D> Iterable<(A, B, C, D) -> Unit>.invokeAll(a: A, b: B, c: C, d: D) {
     for (item in this) {
         item(a, b, c, d)
     }
