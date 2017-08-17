ba62c07bd00a01a1816f7363e31ea0b820fa0f17
Shane <shane@meosphere.com>
2017-08-17 08:50:30 -0600

    function rename.

--- src/main/java/com/lightningkite/kotlin/lambda/Lambda.ext.kt
+++ src/main/java/com/lightningkite/kotlin/lambda/Lambda.ext.kt
@@ -3,6 +3,8 @@ package com.lightningkite.kotlin.lambda
 /**
  * Created by joseph on 11/14/16.
  */
-inline fun <A, B> (() -> A).map(noinline mapper: (A) -> B): () -> B {
+@Deprecated("Use 'transform' instead.", ReplaceWith("transform(mapper)"))
+inline fun <A, B> (() -> A).map(noinline mapper: (A) -> B): () -> B = transform(mapper)
+inline fun <A, B> (() -> A).transform(noinline mapper: (A) -> B): () -> B {
     return { this.invoke().let(mapper) }
 }
